3cbb8647aa682fb51bd37eaf148d266136508c4d
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-03-04 19:58:47 -0600

    Made Numbers buttons and last buttons

--- main.js
+++ main.js
@@ -15,6 +15,26 @@ function crearBtns(){
         }
 
         container.appendChild(btnCleErPer)
-    }
 
+           // Number buttons
+        for(let i = 0; i < 12; i++){
+            let button = document.createElement("button")
+            button.classList.add(`btn${i}`)
+            let textButton = document.createTextNode(i+1)
+    
+            button.appendChild(textButton)
+
+            container.appendChild(button)
+        }
+
+        let lastBtnNumbers = ["9", "10", "11"]
+        let newContent = ["00", 0, "."]
+        for(let i = 0; i < lastBtnNumbers.length; i++){
+            let button = document.querySelector(`.btn${lastBtnNumbers[i]}`);
+            if (button) {
+                button.textContent = newContent[i];
+            }
+        }
+
+    }
 }
