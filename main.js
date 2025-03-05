99aacd8fa96294f1b39cf2bc6001ee3e5e080b42
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-03-04 19:55:22 -0600

    Made Erase, Clear and Percent buttons

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+function crearBtns(){
+    let container = document.querySelector(".container")
+    let btnsCe = ["btnErase", "btnClear", "btnPercent"]
 
+    for(let i = 0; i < btnsCe.length; i++){
+        let btnCleErPer = document.createElement("button")
+        btnCleErPer.classList.add(btnsCe[i])
+
+        if(btnCleErPer.classList.contains(btnsCe[0])){
+            btnCleErPer.textContent = "C"
+        }else if(btnCleErPer.classList.contains(btnsCe[1])){
+            btnCleErPer.textContent = "Delete"
+        }else{
+            btnCleErPer.textContent = "%"
+        }
+
+        container.appendChild(btnCleErPer)
+    }
+
+}
